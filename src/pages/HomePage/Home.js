2dfd1173fa47b48972a33228bfcb53e6ce5c8329
GorAleqsanyann <g.aleqsanyan00@mail.ru>
2025-02-14 15:24:26 +0400

    home Update

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -3,7 +3,11 @@ import CardHome from "../../components/CardHome/CardHome";
 import img1 from "./images/ron1.jpg";
 
 export default function Home() {
-  return <CardHome image={img1} />;
+  return (
+    <div>
+      <CardHome image={img1} />
+    </div>
+  );
 }
 
 //nuyn@ tailwind-ow
